0ce1d078bdd535ebb7de6153e3a1020113dfba74
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-07-16 11:36:04 +0100

    Refactored tests with TestFixture

--- VariousTests/ParserTests.fs
+++ VariousTests/ParserTests.fs
@@ -26,34 +26,45 @@ let ``Parsing anything from an empty string SHOULD fail`` (c: char) =
     result 
     |> checkFailure equal "No more input"
     
-[<Property>]
-let ``Parsing for the correct first char SHOULD return success`` (str: NonEmptyString) =
-    let s = str.Get
-    let firstChar = s.[0]
-    let parser = pchar firstChar;
-
-    let result = run parser s
-    result |> checkSuccess (fun _ _ -> ())
-
-[<Property>]
-let ``Parsing for the correct first char SHOULD return the matched char`` (str: NonEmptyString) =
-    let s = str.Get
-    let firstChar = s.[0]
-    let parser = pchar firstChar;
-
-    let result = run parser s
-    result |> checkSuccess (fun c _ -> c |> should equal firstChar)
-
-[<Property>]
-let ``Parsing for the correct first char SHOULD return the remaining input without the first char`` (str: NonEmptyString) =
-    let s = str.Get
-    match s |> Seq.toList with
-    | [] -> failwith "This should have been a NonEmptyString!"
-    | (firstChar::tail) ->
+type ``Parsing for the correct first char`` () =
+    [<Property>]
+    member x.``SHOULD return success`` (str: NonEmptyString) =
+        let s = str.Get
+        let firstChar = s.[0]
         let parser = pchar firstChar;
 
         let result = run parser s
-        result |> checkSuccess (fun _ s -> s |> should equal (charListToString tail))
+        result |> checkSuccess (fun _ _ -> ())
+
+    [<Property>]
+    member x.``SHOULD return the matched char`` (str: NonEmptyString) =
+        let s = str.Get
+        let firstChar = s.[0]
+        let parser = pchar firstChar;
+
+        let result = run parser s
+        result |> checkSuccess (fun c _ -> c |> should equal firstChar)
+
+    [<Property>]
+    member x.``SHOULD return the remaining input without the first char`` (str: NonEmptyString) =
+        let s = str.Get
+        match s |> Seq.toList with
+        | [] -> failwith "This should have been a NonEmptyString!"
+        | (firstChar::tail) ->
+            let parser = pchar firstChar;
+
+            let result = run parser s
+            result |> checkSuccess (fun _ s -> s |> should equal (charListToString tail))
+
+    [<Property>]
+    member x.``from a one char input string SHOULD succeed and return an empty remaining input`` (c:char) =
+        let s = [c] |> charListToString
+        let parser = pchar c
+        let expected = Success(c, "")
+
+        let result = run parser s
+
+        result |> should equal expected
 
 [<Property>]
 let ``Parsing for the wrong first char SHOULD fail`` (str: NonEmptyString) =
@@ -69,12 +80,3 @@ let ``Parsing for the wrong first char SHOULD fail`` (str: NonEmptyString) =
     result
     |> checkFailure startWith expectedMsg
 
-[<Property>]
-let ``Parsing successfully a one char string SHOULD succeed and return an empty remaining input`` (c:char) =
-    let s = [c] |> charListToString
-    let parser = pchar c
-    let expected = Success(c, "")
-
-    let result = run parser s
-
-    result |> should equal expected
