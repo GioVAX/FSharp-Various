54c9309a5c269b87414ee243a3454c15f20ac9d9
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-07-18 17:42:05 +0100

    mapP tests

---
+++ VariousTests/ParserTests/mapP.fs
@@ -0,0 +1,45 @@
+﻿module mapP
+
+open FsCheck
+
+open ParserTypes
+open ParserMonad
+open Parsers
+
+open ParserTestsUtils
+
+type ``-> mapP`` () =
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        let nums = num |> string
+        let input =  nums + s.Get
+
+        let parser = mapP (int) (pstring nums)
+
+        let result = run parser input
+
+        result
+        |> checkMatched num
+        
+type ``-> mapP infix`` () =
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        let nums = num |> string
+        let input =  nums + s.Get
+
+        let parser = int <!> (pstring nums)
+
+        let result = run parser input
+
+        result
+        |> checkMatched num
+
+type ``-> mapP infix inverted`` () =
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        let nums = num |> string
+        let input =  nums + s.Get
+
+        let parser = (pstring nums) |>> int
+
+        let result = run parser input
+
+        result
+        |> checkMatched num
