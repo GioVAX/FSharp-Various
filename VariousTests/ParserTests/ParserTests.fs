a6b8b2fe1c3e0a1885cc88674e20a4ef6b475202
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-07-18 14:18:16 +0100

    tests for run and cons

--- VariousTests/ParserTests/ParserTests.fs
+++ VariousTests/ParserTests/ParserTests.fs
@@ -1,8 +1,10 @@
 module ParserTests
 
 open FsCheck.Xunit
+open FsUnit.Xunit
 
 open ParserTypes
+open ParserUtils
 open Parsers
 
 open ParserTestsUtils
@@ -15,3 +17,17 @@ let ``Parsing anything from an empty string SHOULD fail for no more input`` (c: 
 
     result 
     |> checkFailure "No more input"
+
+[<Property>]
+let ``run SHOULD call the wrapped function on input`` (s: string) =
+    let f (s:string) = Success (s + s, "")
+
+    run (Parser f) s
+    |> checkMatched (s+s)
+
+[<Property>]
+let ``cons SHOULD return a new list with the new element at the head`` (head: char) (tail: char list) =
+    let result = cons head tail
+    let expected = head::tail
+
+    result |> should equal expected
