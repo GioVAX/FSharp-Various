d2c1287113d1f8388d90e03834670956f557026f
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-07-18 17:47:27 +0100

    Refactored mapP tests

--- VariousTests/ParserTests/mapP.fs
+++ VariousTests/ParserTests/mapP.fs
@@ -9,37 +9,22 @@ open Parsers
 open ParserTestsUtils
 
 type ``-> mapP`` () =
-    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+    let doTest num str parserBuilder =
         let nums = num |> string
-        let input =  nums + s.Get
+        let input =  nums + str
 
-        let parser = mapP (int) (pstring nums)
+        let parser = parserBuilder nums
 
         let result = run parser input
 
         result
         |> checkMatched num
+
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        doTest num s.Get (fun s -> mapP (int) (pstring s))
         
-type ``-> mapP infix`` () =
-    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
-        let nums = num |> string
-        let input =  nums + s.Get
+    member x.``infix SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        doTest num s.Get (fun s -> int <!> (pstring s))
 
-        let parser = int <!> (pstring nums)
-
-        let result = run parser input
-
-        result
-        |> checkMatched num
-
-type ``-> mapP infix inverted`` () =
-    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
-        let nums = num |> string
-        let input =  nums + s.Get
-
-        let parser = (pstring nums) |>> int
-
-        let result = run parser input
-
-        result
-        |> checkMatched num
+    member x.``infix inverted SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        doTest num s.Get (fun s -> (pstring s) |>> int)
