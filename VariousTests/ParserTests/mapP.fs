84832fe5faa7a987098334c48c2ddcf08e2d1930
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-07-19 10:07:50 +0100

    Refactored mapP tests

--- VariousTests/ParserTests/mapP.fs
+++ VariousTests/ParserTests/mapP.fs
@@ -9,55 +9,58 @@ open Parsers
 
 open ParserTestsUtils
 
-type ``-> mapP`` () =
-    let doTest num str parserBuilder checkFn =
-        let nums = num |> string
-        let input =  nums + str
+let private doTest num str parserBuilder checkFn =
+    let nums = num |> string
+    let input =  nums + str
 
-        let parser = parserBuilder nums
+    let parser = parserBuilder nums
 
-        let result = run parser input
+    let result = run parser input
 
-        result
-        |> checkFn num
+    result
+    |> checkFn num
 
-    let failingINput = "Failure"
-    let chkSuccess = checkMatched
-    let chkFail = (fun n -> checkFailure "Expecting")
+let private failingINput = "Failure"
+
+let private chkSuccess = checkMatched
+let private chkFail = (fun n -> checkFailure "Expecting")
+
+type ``-> prefix`` () =
 
     [<Property>]
     member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
         doTest num s.Get 
             (fun s -> mapP (int) (pstring s)) 
             chkSuccess
-        
-    [<Property>]
-    member x.``infix SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
-        doTest num s.Get 
-            (fun s -> int <!> (pstring s))  
-            chkSuccess
-
-    [<Property>]
-    member x.``infix inverted SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
-        doTest num s.Get 
-            (fun s -> (pstring s) |>> int) 
-            chkSuccess
-
 
     [<Property>]
     member x.``SHOULD fail if no matches`` (num: int32) =
         doTest num failingINput 
             (fun s -> mapP (int) (pstring failingINput))  
             chkFail
+        
+type ``-> infix`` () =
+    [<Property>]
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        doTest num s.Get 
+            (fun s -> int <!> (pstring s))  
+            chkSuccess
 
     [<Property>]
-    member x.``infix SHOULD fail if no matches`` (num: int32) =
+    member x.``SHOULD fail if no matches`` (num: int32) =
         doTest num failingINput 
             (fun s -> int <!> (pstring failingINput)) 
             chkFail
 
+type ``-> infix inverted params`` () =
     [<Property>]
-    member x.``infix inverted SHOULD fail if no matches`` (num: int32) (s:NonEmptyString) =
+    member x.``SHOULD transform the result of the parser`` (num: int32) (s:NonEmptyString) =
+        doTest num s.Get 
+            (fun s -> (pstring s) |>> int) 
+            chkSuccess
+
+    [<Property>]
+    member x.``SHOULD fail if no matches`` (num: int32) (s:NonEmptyString) =
         doTest num failingINput 
             (fun s -> (pstring failingINput) |>> int) 
             chkFail
