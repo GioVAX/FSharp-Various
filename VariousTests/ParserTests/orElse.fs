6d1a69b0d55f7335d1d1237164589d858ffe6057
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-08-05 00:01:15 +0100

    orElse infix tests

--- VariousTests/ParserTests/orElse.fs
+++ VariousTests/ParserTests/orElse.fs
@@ -77,3 +77,52 @@ type ``-> prefix`` () =
 
         let result = failingParse orElse c1 c2 c3 str.Get
         result |> checkFailure expectedMsg
+
+type ``-> infix`` () =
+
+    [<Property>]
+    member x.``when first matches SHOULD return the match`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        c1 <> c2
+        ==> lazy
+        let result = successful1stParse (<|>) c1 c2 str.Get
+        result |> checkMatched c1
+
+    [<Property>]
+    member x.``when first matches SHOULD remove just one char`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        c1 <> c2
+        ==> lazy
+        let s = str.Get
+        let expected = (Seq.append [c2] s)
+                        |> Seq.toArray
+                        |> System.String
+
+        let result = successful1stParse (<|>) c1 c2 s
+        result |> checkRemaining expected
+
+    [<Property>]
+    member x.``when second matches SHOULD return the match`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        c1 <> c2
+        ==> lazy
+        let result = successful2ndParse (<|>) c1 c2 str.Get
+        result |> checkMatched c2
+
+    [<Property>]
+    member x.``when second matches SHOULD remove just one char`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        c1 <> c2
+        ==> lazy
+        let s = str.Get
+        let expected = (Seq.append [c1] s)
+                        |> Seq.toArray
+                        |> System.String
+
+        let result = successful2ndParse (<|>) c1 c2 s
+        result |> checkRemaining expected
+
+    [<Property>]
+    member x.``when neither match SHOULD specify the error was the second parser`` (c1:char) (c2:char) (c3:char) (str:NonWhiteSpaceString) =
+        ((c1 <> c2) && (c1 <> c3) && (c2 <> c3))
+        ==> lazy
+        let expectedMsg = sprintf "Expecting '%c'." c2
+
+        let result = failingParse (<|>) c1 c2 c3 str.Get
+        result |> checkFailure expectedMsg
