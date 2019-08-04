d0ac37aceb4c46602d4a92a5a7f4a011b68e7be8
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-08-04 16:47:02 +0100

    Added tests for .>> and >>.

--- VariousTests/ParserTests/concatenateP.fs
+++ VariousTests/ParserTests/concatenateP.fs
@@ -34,7 +34,6 @@ let private failureShort composer c1 c2 =
     |> parse composer c1 c2
 
 type ``-> prefix`` () =
-
     [<Property>]
     member x.``when successful SHOULD return the 2 matches in a tuple`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
         let result = successfulParse concatenateP c1 c2 str.Get
@@ -69,9 +68,7 @@ type ``-> prefix`` () =
         failure2ndParse concatenateP c1 c2 str.Get
         |> checkFailure expectedMsg
 
-
 type ``-> infix`` () =
-
     [<Property>]
     member x.``when successful SHOULD return the 2 matches in a tuple`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
         let result = successfulParse (.>>.) c1 c2 str.Get
@@ -106,4 +103,72 @@ type ``-> infix`` () =
         failure2ndParse (.>>.) c1 c2 str.Get
         |> checkFailure expectedMsg
 
+type ``-> discard second match`` () =
+    [<Property>]
+    member x.``when successful SHOULD return only the first match`` (c1: char) (c2: char) (str:NonWhiteSpaceString) =
+        let result = successfulParse (.>>) c1 c2 str.Get
+        result |> checkMatched c1
 
+    [<Property>]
+    member x.``when successful SHOULD remove both matches from the returned input`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
+        let s = str.Get
+        let result = successfulParse (.>>) c1 c2 s
+        result |> checkRemaining s
+
+    [<Property>]
+    member x.``when input contains only the first parser SHOULD fail for no more input`` (c1: char) (c2:char) =
+        failureShort (.>>) c1 c2
+        |> checkFailure "No more input"
+
+    [<Property>]
+    member x.``when fails on the first parser SHOULD specify the failure was the first parser`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        ((str.Get.Length > 0) && (c1 <> c2))
+        ==> lazy
+        let expectedMsg = sprintf "Expecting '%c'." c1
+        
+        failure1stParse (.>>) c1 c2 str.Get
+        |> checkFailure expectedMsg
+
+    [<Property>]
+    member x.``when fails on the second parser SHOULD specify the failure was the second parser`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
+        (str.Get.Length > 0 && c1 <> c2)
+        ==> lazy
+        let expectedMsg = sprintf "Expecting '%c'." c2
+        
+        failure2ndParse (.>>) c1 c2 str.Get
+        |> checkFailure expectedMsg
+
+type ``-> discard first match`` () =
+    [<Property>]
+    member x.``when successful SHOULD return only the first match`` (c1: char) (c2: char) (str:NonWhiteSpaceString) =
+        let result = successfulParse (>>.) c1 c2 str.Get
+        result |> checkMatched c2
+
+    [<Property>]
+    member x.``when successful SHOULD remove both matches from the returned input`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
+        let s = str.Get
+        let result = successfulParse (>>.) c1 c2 s
+        result |> checkRemaining s
+
+    [<Property>]
+    member x.``when input contains only the first parser SHOULD fail for no more input`` (c1: char) (c2:char) =
+        failureShort (>>.) c1 c2
+        |> checkFailure "No more input"
+
+    [<Property>]
+    member x.``when fails on the first parser SHOULD specify the failure was the first parser`` (c1:char) (c2:char) (str:NonWhiteSpaceString) =
+        ((str.Get.Length > 0) && (c1 <> c2))
+        ==> lazy
+        let expectedMsg = sprintf "Expecting '%c'." c1
+        
+        failure1stParse (>>.) c1 c2 str.Get
+        |> checkFailure expectedMsg
+
+    [<Property>]
+    member x.``when fails on the second parser SHOULD specify the failure was the second parser`` (c1: char) (c2:char) (str:NonWhiteSpaceString) =
+        (str.Get.Length > 0 && c1 <> c2)
+        ==> lazy
+        let expectedMsg = sprintf "Expecting '%c'." c2
+        
+        failure2ndParse (>>.) c1 c2 str.Get
+        |> checkFailure expectedMsg
