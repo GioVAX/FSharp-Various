d02d01c9f727192c914fdf7e6e6e244ad8fa9c2a
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-08-10 22:56:04 +0100

    anyOF tests

--- VariousTests/ParserTests/Choice_AnyOf.fs
+++ VariousTests/ParserTests/Choice_AnyOf.fs
@@ -17,15 +17,15 @@ let parserList f goodChar badChar numParsers =
         |> List.map (fun n -> if n = pos then goodChar else badChar )
         |> f
 
-let successTest builder p (str:string) numParsers =
-    let parsers = builder p str.[0] numParsers
-    let input = sprintf "%c%s" p str
-
-    run (Parsers.choice parsers) input
-
 type ``choice tests`` () =
     
     let parsersBuilder = parserList (List.map pchar)
+
+    let successTest p (str:string) numParsers =
+        let parsers = parsersBuilder p str.[0] numParsers
+        let input = sprintf "%c%s" p str
+
+        run (Parsers.choice parsers) input
 
     [<Property>]
     member x.``a good match in a list of bad matches SHOULD succeed`` (p:char) (s:NonEmptyString) (numParsers:PositiveInt) =
@@ -33,7 +33,7 @@ type ``choice tests`` () =
 
         str |> Seq.exists (fun c' -> c' = p) |> not
         ==> lazy
-            let result = successTest parsersBuilder p str numParsers.Get
+            let result = successTest p str numParsers.Get
         
             result |> checkMatched p
 
@@ -48,6 +48,42 @@ type ``choice tests`` () =
         let input = sprintf "%c%s" p str
 
         let result = run (Parsers.choice parsers) input
+        
+        let expected = sprintf "Expecting '%c'." first
+
+        result |> checkFailure expected
+
+type ``anyOf test`` () =
+
+    let parsersBuilder = parserList id
+
+    let successTest p (str:string) numParsers =
+        let parsers = parsersBuilder p str.[0] numParsers
+        let input = sprintf "%c%s" p str
+
+        run (Parsers.anyOf parsers) input
+
+    [<Property>]
+    member x.``a good match in a list of bad matches SHOULD succeed`` (p:char) (s:NonEmptyString) (numParsers:PositiveInt) =
+        let str = s.Get
+
+        str |> Seq.exists (fun c' -> c' = p) |> not
+        ==> lazy
+            let result = successTest p str numParsers.Get
+        
+            result |> checkMatched p
+
+    [<Property>]
+    member x.``no good match in a list of bad matches SHOULD fail`` (p:char) (s:NonEmptyString) (numParsers:PositiveInt) =
+        let str = s.Get
+
+        str |> Seq.exists (fun c' -> c' = p) |> not
+        ==> lazy
+        let first = str.[0]
+        let parsers = parsersBuilder first first numParsers.Get
+        let input = sprintf "%c%s" p str
+
+        let result = run (Parsers.anyOf parsers) input
         
         let expected = sprintf "Expecting '%c'." first
 
