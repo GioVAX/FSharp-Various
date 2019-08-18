6560eb2bf4943bca85892bcbfbae208a46993747
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-08-18 20:31:53 +0100

    pint tests

---
+++ VariousTests/ParserTests/pint.fs
@@ -0,0 +1,46 @@
+﻿module pint
+
+open FsCheck
+open FsCheck.Xunit
+
+open ParserTypes
+open Parsers
+open ParserTestsUtils
+
+type ``pint tests`` () =
+
+    [<Property>]
+    member x.``numbers SHOULD matched`` (n:int) (s:NonEmptyString) =
+        let s' = s.Get
+
+        ['0'..'9'] |> List.contains s'.[0] |> not
+        ==> lazy
+        let input = sprintf "%i%s" n s'
+        
+        let result = run pint input
+
+        result |> checkMatched n
+
+
+    [<Property>]
+    member x.``numbers SHOULD be removed from input`` (n:int) (s:NonEmptyString) =
+        let s' = s.Get
+
+        ['0'..'9'] |> List.contains s'.[0] |> not
+        ==> lazy
+        let input = sprintf "%i%s" n s'
+        
+        let result = run pint input
+
+        result |> checkRemaining s'
+
+    [<Property>]
+    member x.``lack of numbers SHOULD result in failure`` (s:NonEmptyString) =
+        let s' = s.Get
+
+        '-'::['0'..'9'] |> List.contains s'.[0] |> not
+        ==> lazy
+       
+        let result = run pint s'
+
+        result |> checkFailure "Expecting '9'."
