4afbf1006297e93cd301b70d5538adb1c87cf6ee
Giovanni Pezzino <G.Pezzino@elsevier.com>
2019-08-19 12:32:25 +0100

    separatedBy1 tests

--- VariousTests/ParserTests/separatedBy1.fs
+++ VariousTests/ParserTests/separatedBy1.fs
@@ -1,4 +1,4 @@
-﻿module separatedBy
+﻿module separatedBy1
 
 open FsCheck
 open FsCheck.Xunit
@@ -8,7 +8,7 @@ open Parsers
 
 open ParserTestsUtils
 
-type ``separatedBy tests`` () =
+type ``separatedBy1 tests`` () =
 
     let intChars = '-'::['0'..'9']
 
@@ -21,7 +21,7 @@ type ``separatedBy tests`` () =
                     |> List.fold (+) "" 
                     |> transform
         let input = nums'' + s
-        let parser = separatedBy pint (sep |> pchar)
+        let parser = separatedBy1 pint (sep |> pchar)
 
         let result = run parser input
 
@@ -58,39 +58,26 @@ type ``separatedBy tests`` () =
         test nums' sep s.Get 
 
     [<Property>]
-    member x.``multiple matches with separators except the last oneSHOULD remove all matches and separators from input`` (nums:NonEmptyArray<int>) (sep:char) (s:NonEmptyString) =
+    member x.``multiple matches with separators except the last one SHOULD remove all matches and separators from input`` (nums:NonEmptyArray<int>) (sep:char) (s:NonEmptyString) =
         let s' = s.Get
-        (intChars |> List.contains s'.[0] |> not 
-            && intChars |> List.contains sep |> not
+        (['0'..'9'] |> List.contains s'.[0] |> not 
+            && ['0'..'9'] |> List.contains sep |> not
             && s'.[0] <> sep)
         ==> lazy
         let test = successfulTest removeLastSeparator (checkRemaining s')
         test nums.Get sep s' 
 
     [<Property>]
-    member x.``no matches with separators SHOULD succeed and match nothing`` (s:NonEmptyString) =
+    member x.``no matches with separators SHOULD fail`` (s:NonEmptyString) =
         let s' = s.Get
         (intChars |> List.contains s'.[0] |> not )
         ==> lazy
         let sep = match s'.[0] with
                     | 'a' -> 'b'
                     | _ -> 'a'
-        let parser = separatedBy pint (sep |> pchar)
+        let parser = separatedBy1 pint (sep |> pchar)
+        let expected = sprintf "Expecting '9'."
 
         let result = run parser s'
 
-        result |> checkMatched List.empty<int>
-
-    [<Property>]
-    member x.``no matches with separators SHOULD leave input unchanged`` (s:NonEmptyString) =
-        let s' = s.Get
-        (intChars |> List.contains s'.[0] |> not )
-        ==> lazy
-        let sep = match s'.[0] with
-                    | 'a' -> 'b'
-                    | _ -> 'a'
-        let parser = separatedBy pint (sep |> pchar)
-
-        let result = run parser s'
-
-        result |> checkRemaining s'
+        result |> checkFailure expected
