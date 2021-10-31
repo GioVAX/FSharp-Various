34944661c451a806dfdd462777ae135c1a07e133
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-10-31 15:06:01 +0100

    Trie - first implementation

--- VariousTests/trieTests.fs
+++ VariousTests/trieTests.fs
@@ -6,7 +6,23 @@ open Xunit
 open Trie
 
 [<Fact>]
-let ``first test`` () =
-    // let res = lcs "BMOAL" "BLOA"
-    // res |> should equal "BOA"
-    ()
+let ``empty trie`` () =
+    let root = Trie.empty
+
+    root.Branches |> should haveCount 0
+    root.EndOfWord |> should be False
+
+[<Fact>]
+let ``add a 1-char string to an empty trie`` () =
+    let root = Trie.empty
+    let tree = root |> Trie.add "s"
+
+    tree.Branches 
+        |> should haveCount 1
+
+    let sNode = tree.Branches |> Map.tryFind 's'
+    sNode.IsSome |> should be True
+
+    let node = sNode |> Option.get
+    node.Branches |> should haveCount 0
+    node.EndOfWord |> should be True
--- trie.fs
+++ trie.fs
@@ -4,3 +4,28 @@ type Trie = {
     Branches: Map<char,Trie>
     EndOfWord : bool
 }
+
+let empty = { 
+    Branches = Map.empty
+    EndOfWord = false
+}
+
+let add s root =
+    let rec loop root = function
+    | [c] -> 
+        let newNode = 
+            match root.Branches.TryFind c with
+            | Some node -> node
+            | None -> empty
+        {root with Branches = root.Branches.Add (c, {newNode with EndOfWord = true})}
+    | c::tail ->
+        let newNode = 
+            match root.Branches.TryFind c with
+            | Some node -> 
+                loop node tail
+            | None -> 
+                loop empty tail
+        {root with Branches = root.Branches.Add (c, newNode)}
+    | _ -> root
+
+    loop root (s |> Seq.toList)
